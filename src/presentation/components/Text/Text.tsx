18db74e656c0a3a6346476dac0e33e469c3f50e2
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-19 13:31:20 -0300

    refactor(component): add weight modifier in Text

--- src/presentation/components/Text/Text.tsx
+++ src/presentation/components/Text/Text.tsx
@@ -4,8 +4,9 @@ import '@/presentation/components/Text/Text.scss'
 
 interface TextProps extends ComponentProps<HTMLParagraphElement> {
   size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'xxxl'
+  weigth?: 'regular' | 'medium' | 'semi-bold' | 'bold';
 }
 
-export function Text({ children, size }: TextProps) {
-  return <BaseTypography className={`text text--${size}`}>{children}</BaseTypography>
+export function Text({ children, size, weigth = 'regular' }: TextProps) {
+  return <BaseTypography className={`text text--${size} text--${weigth}`}>{children}</BaseTypography>
 }
