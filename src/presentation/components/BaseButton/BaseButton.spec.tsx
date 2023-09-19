d283d96359bdc7cc5b3fe40e39dd2c120c1ead8f
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-19 15:30:55 -0300

    test(component): add BaseButton

---
+++ src/presentation/components/BaseButton/BaseButton.spec.tsx
@@ -0,0 +1,22 @@
+import { screen, render } from '@testing-library/react'
+import { BaseButton } from '@/presentation/components/BaseButton'
+import { DiscordLogoIcon } from '@radix-ui/react-icons'
+
+describe('BaseButton', () => {
+  it('should render component with correct modifiers', () => {
+    render(<BaseButton>valid-text</BaseButton>)
+    const sut = screen.getByRole('button')
+
+    expect(sut).toBeTruthy()
+    expect(sut.className).toContain('base-button base-button--primary')
+  })
+
+  it('should render component with icon', () => {
+    render(<BaseButton icon={<DiscordLogoIcon />}>valid-text</BaseButton>)
+    const sut = screen.getByRole('img')
+
+    screen.debug()
+    expect(sut).toBeTruthy()
+    expect(sut.className).toContain('base-button__icon')
+  })
+})
