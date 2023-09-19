9a75c9ff38bca03aeb7e3ec04c22dd8624375844
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-19 17:48:10 -0300

    feat(component): add BaseContainer

---
+++ src/presentation/components/BaseContainer/BaseContainer.tsx
@@ -0,0 +1,18 @@
+import classNames from 'classnames'
+import { ComponentProps } from '@/presentation/components/core'
+import '@/presentation/components/BaseContainer/BaseContainer.scss'
+
+interface BaseContainerProps extends ComponentProps<HTMLDivElement> {
+  variant?: 'primary' | 'secondary'
+}
+
+export function BaseContainer({ children, variant = 'primary', className, ...others }: BaseContainerProps) {
+  return (
+    <div
+      className={classNames('base-container', `base-container--${variant}`, className)}
+      {...others}
+    >
+      {children}
+    </div>
+  )
+}
