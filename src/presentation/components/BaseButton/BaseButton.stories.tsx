ab1374433c38228fd5e6688c9dc3280255c344a9
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-19 15:31:07 -0300

    story(component): add BaseButton

---
+++ src/presentation/components/BaseButton/BaseButton.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { BaseButton } from '@/presentation/components/BaseButton'
+import { DiscordLogoIcon } from '@radix-ui/react-icons'
+
+const meta: Meta<typeof BaseButton> = {
+  component: BaseButton,
+  argTypes: { onClick: { action: 'clicked' } }
+}
+
+export default meta
+type Story = StoryObj<typeof BaseButton>
+
+export const Default: Story = {
+  args: { children: 'valid-text' }
+}
+
+export const Icon: Story = {
+  args: { children: 'valid-text', icon: <DiscordLogoIcon /> }
+}
