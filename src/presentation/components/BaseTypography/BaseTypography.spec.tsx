99da10a9865349858092d815853077933a25cdbd
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-19 12:51:43 -0300

    test(component): add base-typography

---
+++ src/presentation/components/BaseTypography/BaseTypography.spec.tsx
@@ -0,0 +1,12 @@
+import { screen, render } from '@testing-library/react'
+import { BaseTypography } from '@/presentation/components/BaseTypography'
+
+describe('BaseTypography', () => {
+  it('should render component', () => {
+    render(<BaseTypography>valid-text</BaseTypography>)
+    const sut = screen.getByText('valid-text')
+
+    expect(sut).toBeTruthy()
+    expect(sut.className).toEqual('base-typography')
+  })
+})
