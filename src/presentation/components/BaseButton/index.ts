efbf54b99420a9bf3a3dd5c7d07c2a01c7679aee
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-19 15:30:38 -0300

    feat(component): add BaseButton

---
+++ src/presentation/components/BaseButton/index.ts
@@ -0,0 +1,1 @@
+export * from '@/presentation/components/BaseButton/BaseButton.tsx'
--- src/presentation/components/Text/Text.tsx
+++ src/presentation/components/Text/Text.tsx
@@ -4,9 +4,14 @@ import '@/presentation/components/Text/Text.scss'
 
 interface TextProps extends ComponentProps<HTMLParagraphElement> {
   size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'xxxl'
-  weigth?: 'regular' | 'medium' | 'semi-bold' | 'bold';
+  weigth?: 'regular' | 'medium' | 'semi-bold' | 'bold'
+  color?: 'white' | 'black' | 'grey'
 }
 
-export function Text({ children, size, weigth = 'regular' }: TextProps) {
-  return <BaseTypography className={`text text--${size} text--${weigth}`}>{children}</BaseTypography>
+export function Text({ children, size, weigth = 'regular', color = 'black', ...others }: TextProps) {
+  return (
+    <BaseTypography className={`text text--${size} text--${weigth} text--color-${color}`} {...others}>
+      {children}
+    </BaseTypography>
+  )
 }
