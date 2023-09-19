b13ceb1da255a575fb0b9e7643740aa1bd02f7d0
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-19 16:28:04 -0300

    story(component): add Avatar

---
+++ src/presentation/components/Avatar/Avatar.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar } from '@/presentation/components/Avatar'
+
+const meta: Meta<typeof Avatar> = {
+  component: Avatar
+}
+
+export default meta
+type Story = StoryObj<typeof Avatar>
+
+export const Default: Story = {
+  args: { src: 'https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80', fallback: 'AD' }
+}
+
+export const FallBack: Story = {
+  args: { src: '', fallback: 'AD' }
+}
