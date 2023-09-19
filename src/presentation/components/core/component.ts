7b959b2d58aa9b9099f01cb1cab588af81530785
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-19 13:23:28 -0300

    chore(package): add classNames

--- src/presentation/components/core/component.ts
+++ src/presentation/components/core/component.ts
@@ -2,4 +2,5 @@ import { HTMLAttributes, ReactNode } from 'react'
 
 export interface ComponentProps<T = HTMLElement> extends HTMLAttributes<T> {
   children?: ReactNode | undefined
+  className?: string
 }
