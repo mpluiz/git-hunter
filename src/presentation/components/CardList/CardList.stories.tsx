1dcdac3faa8ebfe2b69be2f25905e697ae35a527
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-19 18:42:24 -0300

    story(component): add CardList

---
+++ src/presentation/components/CardList/CardList.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { CardList } from '@/presentation/components/CardList/CardList.tsx'
+
+const meta: Meta<typeof CardList> = {
+  component: CardList
+}
+
+export default meta
+type Story = StoryObj<typeof CardList>
+
+export const Default: Story = {
+  args: {
+    items: [
+      { name: 'item1' },
+      { name: 'item1' },
+      { name: 'item1' },
+      { name: 'item1' }
+    ]
+  }
+}
