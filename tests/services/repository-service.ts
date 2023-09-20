d91f4e5ba669b12cebf160166b9a3bd3a6193a0e
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-20 13:41:44 -0300

    test(service): add GetByName in InMemoryRepositoryService

--- tests/services/repository-service.ts
+++ tests/services/repository-service.ts
@@ -4,6 +4,11 @@ import { Repository } from '@/domain/entities/repository.ts'
 export class InMemoryRepositoryService implements IRepositoryService {
   public repositories: Repository[] = []
 
+  async getByName(name: string): Promise<Repository | null> {
+    const index = this.repositories.findIndex(repository => repository.name === name)
+    return this.repositories[index]
+  }
+
   async fetchRepositoriesByUserName(username: string): Promise<Repository[] | null> {
     this.repositories.findIndex(repository => repository.name === username)
     return this.repositories
