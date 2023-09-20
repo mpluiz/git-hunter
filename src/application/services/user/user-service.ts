69a4f5c7d168fd84f6997fbc572d62ee944a473e
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-20 15:42:46 -0300

    feat(application): add UserService implementation

--- src/application/services/user/user-service.ts
+++ src/application/services/user/user-service.ts
@@ -1,5 +1,22 @@
 import { User } from '@/domain/entities/user.ts'
+import { HttpClient } from '@/application/protocols/http/http-client.ts'
 
 export interface IUserService {
   getByName(username: string) : Promise<User | null>
+}
+
+export class UserService implements IUserService {
+  constructor(private readonly client: HttpClient) {}
+
+  async getByName(username: string): Promise<User | null> {
+    const response = await this.client.http.get(`/${username}`)
+
+    if (!response.data) {
+      return null
+    }
+
+    const { id, name, followers, following, bio, avatar_url: avatarUrl, html_url: profileUrl } = response.data
+
+    return User.create({ id, name, followers, following, bio, avatarUrl, profileUrl })
+  }
 }
