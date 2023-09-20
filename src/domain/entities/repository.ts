e2d7ea26e1d655cd00db2296bcc1efcf9276455c
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-20 13:20:09 -0300

    feat(domain): add repository entity

---
+++ src/domain/entities/repository.ts
@@ -0,0 +1,20 @@
+import { Entity } from '@/domain/entities/entity.ts'
+
+export interface RepositoryProps {
+  id: number
+  name: string
+  stars: number
+  language: string
+  description?: string
+  repositoryUrl?: string
+}
+
+export class Repository extends Entity<RepositoryProps> {
+  static create(props: RepositoryProps): Repository {
+    return new Repository(props)
+  }
+
+  get name(): string {
+    return this.props.name
+  }
+}
