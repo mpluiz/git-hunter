9308423f571f3381a865e7105052a536ff24822a
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-20 17:06:07 -0300

    feat(application): add UseMap

---
+++ src/application/mappers/user.ts
@@ -0,0 +1,16 @@
+import { Mapper } from '@/application/mappers/mapper.ts'
+import { User } from '@/domain/entities/user.ts'
+
+export class UserMap extends Mapper {
+  static toDomain(raw: any): User {
+    return User.create({
+      id: raw.id,
+      name: raw.name,
+      followers: raw.followers,
+      following: raw.following,
+      bio: raw.bio,
+      avatarUrl: raw.avatar_url,
+      profileUrl: raw.html_url
+    })
+  }
+}
