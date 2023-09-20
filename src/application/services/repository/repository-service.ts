4c5aab7a6f15be2db5f4e3327c22f4379502127b
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-20 17:06:52 -0300

    feat(application): add RepositoryService implementation

--- src/application/services/repository/repository-service.ts
+++ src/application/services/repository/repository-service.ts
@@ -1,6 +1,32 @@
 import { Repository } from '@/domain/entities/repository.ts'
+import { HttpClient } from '@/application/protocols/http/http-client.ts'
+import { RepositoryMap } from '@/application/mappers/repository.ts'
 
 export interface IRepositoryService {
   fetchRepositoriesByUserName(username: string) : Promise<Repository[] | null>
-  getByName(name: string) : Promise<Repository | null>
+  getByName(repositoryFullName: string) : Promise<Repository | null>
+}
+
+export class RepositoryService implements IRepositoryService {
+  constructor(private readonly client: HttpClient) {}
+
+  async getByName(repositoryFullName: string): Promise<Repository | null> {
+    const response = await this.client.http.get(`/repos/${repositoryFullName}`)
+
+    if (!response.data) {
+      return null
+    }
+
+    return RepositoryMap.toDomain(response.data)
+  }
+
+  async fetchRepositoriesByUserName(username: string): Promise<Repository[] | null> {
+    const response = await this.client.http.get(`/users/${username}/repos`)
+
+    if (!response.data) {
+      return null
+    }
+
+    return response.data.map((data: any) => RepositoryMap.toDomain(data))
+  }
 }
