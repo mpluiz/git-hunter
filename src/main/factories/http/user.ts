4141717c326813decbd8d227d6f91624695d6cb0
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-20 15:38:31 -0300

    feat(main/factory): add makeUserHttpClient factory

---
+++ src/main/factories/http/user.ts
@@ -0,0 +1,8 @@
+import { HttpClient } from '@/application/protocols/http/http-client.ts'
+import { AxiosHttpClient } from '@/infra/http/axios-http-client.ts'
+
+export function makeUserHttpClient(): HttpClient {
+  const path = 'users'
+  const baseUrl = import.meta.env.VITE_GITHUB_BASE_URL
+  return new AxiosHttpClient({ baseUrl, path })
+}
