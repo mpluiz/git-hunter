45b4b1b82f15afb29d2e0c365b76386f50e7b111
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-19 21:56:52 -0300

    chore(assets): update styles folder location

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import '@/presentation/styles/normalize.css'
-import '@/presentation/styles/global.css'
-import '@/presentation/styles/variables.css'
+import '@/presentation/assets/styles/normalize.css'
+import '@/presentation/assets/styles/global.css'
+import '@/presentation/assets/styles/variables.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
