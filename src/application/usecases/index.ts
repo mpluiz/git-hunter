0e8521ed0c405eb078756671c3fefd3bbd796fdb
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-20 13:41:10 -0300

    feat(application): add GetRepositoryUseCase implementation

--- src/application/usecases/index.ts
+++ src/application/usecases/index.ts
@@ -1,2 +1,3 @@
 export * from '@/application/usecases/user/get-user.ts'
 export * from '@/application/usecases/repository/fetch-repositories.ts'
+export * from '@/application/usecases/repository/get-repository.ts'
