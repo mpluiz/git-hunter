adba090fd1bdc3f00f9afa9bfc25fcab5b76fde5
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-21 15:20:02 -0300

    feat(application): add error handling in UserService

--- src/application/services/user/user-service.ts
+++ src/application/services/user/user-service.ts
@@ -1,21 +1,23 @@
-import { User } from '@/domain/entities/user.ts'
 import { HttpClient } from '@/application/protocols/http/http-client.ts'
 import { UserMap } from '@/application/mappers/user.ts'
+import { User } from '@/domain/entities/user.ts'
+import { Either, failure, success } from '@/utils/either.ts'
 
 export interface IUserService {
-  getByName(username: string) : Promise<User | null>
+  getByName(username: string): Promise<Either<any, { user: User }>>
 }
 
 export class UserService implements IUserService {
   constructor(private readonly client: HttpClient) {}
 
-  async getByName(username: string): Promise<User | null> {
-    const response = await this.client.http.get(`/${username}`)
+  async getByName(username: string): Promise<Either<any, { user: User }>> {
+    try {
+      const response = await this.client.http.get(`/${username}`)
 
-    if (!response.data) {
-      return null
+      return success({ user: UserMap.toDomain(response.data) })
+    } catch (error: any) {
+      // TODO: add handler
+      return failure(error)
     }
-
-    return UserMap.toDomain(response.data)
   }
 }
