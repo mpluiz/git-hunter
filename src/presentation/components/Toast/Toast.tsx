5952244a2d8fae3b9c01fcef3e2983dbeb58d8db
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-21 15:54:58 -0300

    feat(component): add Toast

---
+++ src/presentation/components/Toast/Toast.tsx
@@ -0,0 +1,30 @@
+import { Dispatch, SetStateAction } from 'react'
+import { ComponentProps } from '@/presentation/components/core'
+import { Text } from '@/presentation/components/Text'
+import * as RToast from '@radix-ui/react-toast'
+import '@/presentation/components/Toast/Toast.scss'
+
+interface ToastProps extends ComponentProps<HTMLParagraphElement> {
+  variant: 'primary' | 'danger'
+  title: string
+  description?: string
+  open: boolean
+  setOpen: Dispatch<SetStateAction<boolean>>
+}
+
+export function Toast({ variant, title, description, open, setOpen }: ToastProps) {
+  return (
+    <RToast.Provider>
+      <RToast.Root className={`toast toast--${variant}`} open={open} onOpenChange={setOpen}>
+        <RToast.Title className="toast__title">
+          <Text size="md" weigth="bold" color="danger">{title}</Text>
+        </RToast.Title>
+        <RToast.Description className="toast__description">
+          <Text size="sm" weigth="regular" color="danger">{description}</Text>
+        </RToast.Description>
+      </RToast.Root>
+
+      <RToast.Viewport className="toast__viewport" />
+    </RToast.Provider>
+  )
+}
