31e7553c14efd9d67245e651bb332c5766de53e7
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-21 15:29:00 -0300

    feat(main/route): implements UseContext and Page Factories

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,10 +1,20 @@
-import { BrowserRouter } from 'react-router-dom'
-import { Router } from '@/presentation/routes/Router.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { UserContext } from '@/presentation/hooks/context/user.ts'
+import { getCurrentUser } from '@/main/adapters/user/current-user.ts'
+import { MakeHomePage, MakeRepositoryDetailsPage, MakeUserDetailsPage } from '@/main/factories/pages'
 
 export function MainRouter() {
   return (
     <BrowserRouter>
-      <Router/>
+      <UserContext.Provider value={{ getCurrentUser }}>
+
+        <Routes>
+          <Route path="/" element={<MakeHomePage />} />
+          <Route path="/users/:user/details" element={<MakeUserDetailsPage />} />
+          <Route path="/repositories/:repository/details" element={<MakeRepositoryDetailsPage />} />
+        </Routes>
+
+      </UserContext.Provider>
     </BrowserRouter>
   )
 }
