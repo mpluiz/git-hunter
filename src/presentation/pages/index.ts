a41586efbaf2cef3de8c81e0a88fd2fdc27caf52
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-21 15:28:00 -0300

    feat(main/factory): add MakeRepositoryDetailsPage

---
+++ src/presentation/pages/index.ts
@@ -0,0 +1,3 @@
+export * from '@/presentation/pages/Home/Home.tsx'
+export * from '@/presentation/pages/UserDetails/UserDetails.tsx'
+export * from '@/presentation/pages/RepositoryDetails/RepositoryDetails.tsx'
