a9e45413af8b957b148c2c3e43037f4765c5b09a
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-21 16:46:56 -0300

    story(component): add TextField

---
+++ src/presentation/components/TextField/TextField.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { TextField } from '@/presentation/components/TextField'
+import { BaseButton } from '@/presentation/components/BaseButton'
+
+const meta: Meta<typeof TextField> = {
+  component: TextField,
+  title: 'Form/TextField',
+  argTypes: { onChange: { action: 'change' } }
+}
+
+export default meta
+type Story = StoryObj<typeof TextField>
+
+export const Default: Story = {
+  args: { name: 'user', placeholder: 'Digite o username do Github' },
+  render: (args) => (
+    <div style={{ display: 'flex', flexDirection: 'row', gap: '8px' }}>
+      <TextField {...args} />
+      <BaseButton size="lg">Buscar</BaseButton>
+    </div>
+  )
+}
