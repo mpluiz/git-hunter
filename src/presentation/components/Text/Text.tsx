d4e48afa8cc2b8bc99cfe452dda75de1b3855e2a
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-21 15:09:17 -0300

    feat(component): add danger color in Text

--- src/presentation/components/Text/Text.tsx
+++ src/presentation/components/Text/Text.tsx
@@ -5,7 +5,7 @@ import '@/presentation/components/Text/Text.scss'
 interface TextProps extends ComponentProps<HTMLParagraphElement> {
   size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'xxxl'
   weigth?: 'regular' | 'medium' | 'semi-bold' | 'bold'
-  color?: 'white' | 'black' | 'grey'
+  color?: 'white' | 'black' | 'grey' | 'danger'
 }
 
 export function Text({ children, size, weigth = 'regular', color = 'black', ...others }: TextProps) {
