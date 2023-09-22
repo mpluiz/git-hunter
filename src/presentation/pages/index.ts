9860a78650bb2477c5ea9c4e47035efa5324badb
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-22 13:15:38 -0300

    deploy: add netlify config

--- src/presentation/pages/index.ts
+++ src/presentation/pages/index.ts
@@ -1,3 +1,2 @@
 export * from '@/presentation/pages/Home/Home.tsx'
-export * from '@/presentation/pages/UserDetails/UserDetails.tsx'
 export * from '@/presentation/pages/RepositoryDetails/RepositoryDetails.tsx'
