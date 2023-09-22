5e662b751c75f8cc3c4f1144ad93c7d69d2fd1ac
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-22 12:44:52 -0300

    story(component): add RepositoriesList

---
+++ src/presentation/components/RepositoryList/RepositoriesList.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { withRouter } from 'storybook-addon-react-router-v6'
+import { RepositoriesList } from '@/presentation/components'
+import { RepositoryProps } from '@/domain/entities/repository.ts'
+
+const meta: Meta<typeof RepositoriesList> = {
+  component: RepositoriesList,
+  decorators: [withRouter]
+}
+
+const repositoriesMock:RepositoryProps[] = [
+  {
+    id: 502686438,
+    name: 'clean-ts-api',
+    description: 'API em NodeJs usando Typescript, TDD, Clean Architecture, Design Patterns e SOLID principles',
+    fullName: 'mpluiz/clean-ts-api',
+    stars: 0,
+    repositoryUrl: 'https://github.com/mpluiz/clean-ts-api'
+  },
+  {
+    id: 389230718,
+    name: 'dock-app',
+    language: 'Python',
+    description: 'API REST para sistema de gestão de contas',
+    fullName: 'mpluiz/dock-app',
+    stars: 0,
+    repositoryUrl: 'https://github.com/mpluiz/dock-app'
+  },
+  {
+    id: 649737763,
+    name: 'dotfiles',
+    fullName: 'mpluiz/dotfiles',
+    stars: 0,
+    repositoryUrl: 'https://github.com/mpluiz/dotfiles'
+  }
+]
+
+async function fetchRepositoriesMock(sort: string) {
+  console.log(sort)
+}
+
+export default meta
+type Story = StoryObj<typeof RepositoriesList>
+
+export const Default: Story = {
+  args: { repositories: repositoriesMock, fetchRepositories: fetchRepositoriesMock }
+}
