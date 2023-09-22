b5ecd809aa49d8dd56f7e0334079d4890ed78788
mpluiz <marcospaulo.luiz@gmail.com>
2023-09-22 12:38:48 -0300

    feat(component): add NavBar

---
+++ src/presentation/components/NavBar/NavBar.tsx
@@ -0,0 +1,38 @@
+import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { ArrowLeftIcon, HomeIcon, PersonIcon } from '@radix-ui/react-icons'
+import { BaseButton, BaseContainer, ComponentProps, Text } from '@/presentation/components'
+import '@/presentation/components/NavBar/NavBar.scss'
+import { UserProps } from '@/domain/entities/user.ts'
+
+export interface NavBarProps extends ComponentProps<HTMLDivElement> {
+  currentUser: UserProps
+}
+
+export function NavBar({ currentUser }: NavBarProps) {
+  const navigate = useNavigate()
+
+  return (
+    <BaseContainer className="nav-bar">
+      <div className="nav-bar__actions">
+        <BaseButton onClick={() => navigate(-1)}>
+          <ArrowLeftIcon/>
+        </BaseButton>
+
+        <Link to="/">
+          <BaseButton>
+            <HomeIcon/>
+          </BaseButton>
+        </Link>
+
+        <NavLink to={`/users/${currentUser.userName}/details`}>
+          <BaseButton>
+            <PersonIcon />
+          </BaseButton>
+        </NavLink>
+
+      </div>
+
+      <Text>GIT HUNTER</Text>
+    </BaseContainer>
+  )
+}
